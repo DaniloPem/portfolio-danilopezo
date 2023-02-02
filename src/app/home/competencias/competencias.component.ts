cfe3964c5a2c597617d553d17153e7a7170e730e
Danilo Pezo <danilo_97pem@hotmail.com>
2023-02-02 17:24:13 -0300

    Realizado a melhora do código no componente competencias

--- src/app/home/competencias/competencias.component.ts
+++ src/app/home/competencias/competencias.component.ts
@@ -5,26 +5,47 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-competencias',
   templateUrl: './competencias.component.html',
-  styleUrls: ['./competencias.component.scss']
+  styleUrls: ['./competencias.component.scss'],
 })
 export class CompetenciasComponent implements OnInit {
 
-  niveis = ['10', '9','8','7','6','5','4','3','2','1'];
-  competencias = [
-    {nome: 'html5', nivel: 'n7' }, {nome: 'css3', nivel: 'n7'},
-    {nome: 'angular', nivel: 'n5'}, {nome: 'javascript', nivel: 'n7'},
-    {nome: 'typescript', nivel: 'n7'}, {nome: 'java', nivel: 'n3'},
-    {nome: 'poo', nivel: 'n7'}, {nome: 'mysql', nivel: 'n3'},
-    {nome: 'espanhol', nivel: 'n10'}, {nome: 'portugues', nivel: 'n8'},
-    {nome: 'ingles', nivel: 'n2'}
+  competenciasWrapper = [
+    {
+      nome: 'Front-end',
+      competencias: [
+        { nome: 'html5', nivel: '70%' },
+        { nome: 'css3', nivel: '70%' },
+        { nome: 'angular', nivel: '50%' },
+        { nome: 'javascript', nivel: '70%' },
+        { nome: 'typescript', nivel: '70%' },
+        { nome: 'java', nivel: '30%' },
+      ],
+      classeCss: 'front-end-competencia'
+    },
+    {
+      nome: 'Programacao',
+      competencias: [{ nome: 'poo', nivel: '70%' }],
+      classeCss: 'programacao-competencia'
+    },
+    {
+      nome: 'Banco de dados',
+      competencias: [{ nome: 'mysql', nivel: '30%' }],
+      classeCss: 'banco-de-dados-competencia'
+    },
+  ];
+  competenciasKeys = Object.keys(this.competenciasWrapper);
+
+  idiomas = [
+    { nome: 'espanhol', nivel: '100%' },
+    { nome: 'portugues', nivel: '80%' },
+    { nome: 'ingles', nivel: '20%' },
   ];
 
   idioma!: Observable<string>;
 
-  constructor(private idiomasService: IdiomasService) { }
+  constructor(private idiomasService: IdiomasService) {}
 
   ngOnInit(): void {
     this.idioma = this.idiomasService.getIdioma();
   }
-
 }
